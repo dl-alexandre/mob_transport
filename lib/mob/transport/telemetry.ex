bc6e6d3dfe8087c978bb4ed5a3f44e73dd79874c
dl-alexandre <166029845+dl-alexandre@users.noreply.github.com>
2026-06-08 08:33:17 -0700

    Reconcile embedded transport package state

--- lib/mob/transport/telemetry.ex
+++
@@ -1,8 +0,0 @@
-defmodule Mob.Transport.Telemetry do
-  @moduledoc false
-
-  @spec emit(atom() | [atom()], map(), map()) :: :ok
-  def emit(event, measurements \\ %{}, metadata \\ %{}) do
-    :telemetry.execute([:mob, :transport] ++ List.wrap(event), measurements, metadata)
-  end
-end
